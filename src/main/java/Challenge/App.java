feb9ba9f106db28bb08b4875a4e79b519ee008a2
Peachey-Desktop <the1peachey@gmail.com>
2021-06-05 18:23:09 -0400

    Initial commit

---
+++ src/main/java/Challenge/App.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 John Peachey
+ */
+
+package Challenge;
+
+/*
+Write a program that converts currency. Specifically, convert euros to U.S.
+dollars. Prompt for the amount of money in euros you have, and prompt for
+the current exchange rate of the euro. Print out the new amount in U.S. dollars.
+
+The formula for currency conversion is:
+amount_to = (amount_from * rate_from) / rate_to
+
+where:
+amount_to is the amount in U.S. dollars.
+amount_from is the amount in euros.
+rate_from is the current exchange rate in euros.
+rate_to is the current exchange rate of the U.S. dollar.
+
+Example Output:
+How many euros are you exchanging? 81
+What is the exchange rate? 137.51
+81 euros at an exchange rate of 137.51 is
+111.38 U.S. dollars.
+
+Constraints:
+Ensure that fractions of a cent are rounded up to the next penny.
+Use a single output statement.
+
+Challenges:
+*Build a dictionary of conversion rates and prompt for the countries
+instead of the rates.
+*Wire up your application to an external API that provides the current
+exchange rates.
+ */
+
+public class App {
+}
